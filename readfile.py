d0fe9f9b93d22b0a23426399658112fad0ab495a
ayushi24ai <98868155+ayushi24ai@users.noreply.github.com>
2022-02-05 14:25:04 +0530

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -6,7 +6,3 @@ for lines in data:
     count += 1
 print ('total number of lines:',count)
 
-import os
-file_size = os.path.getsize("hii.txt")
-print("\nThe size of hii.txt is :",file_size,"Bytes")
-
